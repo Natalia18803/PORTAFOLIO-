8c75eb04c59cbb77821afd224ed11b0b354ffe38
Natalia <natisss820@gmail.com>
2025-07-22 20:18:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+
+
+document.getElementById("botton1").addEventListener("click", function() {
+    window.location.href = "http://127.0.0.1:5501/PAGINA%20ELECTRODOMESTICOS/PAGINA%20ELECTRODOMESTICOS/index.html";
+});
